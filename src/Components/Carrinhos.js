392d75b7c715e47b0936422e34d371988b40ee42
Pamela <pamelarane@gmail.com>
2021-05-12 17:17:21 -0300

    Carrinho do Projeto

--- src/Components/Carrinhos.js
+++ src/Components/Carrinhos.js
@@ -1,13 +1,41 @@
 import React from 'react';
+//import styled from 'styled-components';
 
 
+export default class carrinhos extends React.Component{
 
-function Carrinhos() {
+  render() {
+
     return (
       <div >
-        <p>Teste 1</p>
+        <h3>Carrinho:</h3>
+        <div>
+        <p>1x Viagem Cidade Espacial I </p>
+        <button>Remover</button>
+  
+        </div>
+        <div>
+        <p>1x "Viagem à Lua  </p>
+        <button>Remover</button>
+  
+        </div>
+        <div>
+        <p>1x Viagem à Marte </p>
+        <button>Remover</button>
+  
+        </div>
+        
+  
+  
+        <p>Valor total R$ 100,00</p>
+  
+        <button>Finalizar compra</button>
+  
       </div>
     );
+        
   }
-  
-  export default Carrinhos;
+    
+
+}
+ 
