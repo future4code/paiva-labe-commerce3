ab83eaf0ce9b56863aab5fbff79b65e6c8481d3c
Pamela <pamelarane@gmail.com>
2021-05-12 22:34:46 -0300

    carrinho

--- src/Components/Carrinhos.js
+++ src/Components/Carrinhos.js
@@ -1,41 +1,109 @@
 import React from 'react';
-//import styled from 'styled-components';
+import styled from 'styled-components';
+
+const DentrodoCarrinho = styled.div`
+width: 20vw;
+height: 30vw;
+display: flex;
+//flex:1;
+padding: 8px;
+border: 1px solid black; 
+justify-content: flex-end;
+//justify-items: center;
+`
+
+
 
 
 export default class carrinhos extends React.Component{
 
+  state = {
+    produtos:[],
+    remover:""
+  }
+
+    onClickRemover = (idDeletar) =>{
+    if(window.confirm ()){
+        const removerProduto = [...this.state.produtos]
+        const produtoRemovido = removerProduto.filter((produtos)=>{
+
+            return produtos.index !== idDeletar ;
+        })
+
+        this.setState({produtos: produtoRemovido})
+    }   else{
+      
+        return
+        }
+        
+    }
+
+
+
+    carrinhoVazio = () =>{
+      const vazio = {
+        produtos: this.state.produtos,
+        semProdutos: this.state.produtos,
+        index: Math.random()
+      }
+      if(vazio.produtos === "" ){
+        alert("Carrinho esta vazio")
+        return
+      }
+
+      
+
+    }
+
+
+   
+    compraRealizada = () =>{
+
+      /* colocar efeito de obrigada pela compra, vai pesquisar como kk */
+
+
+    }
+
+
   render() {
 
     return (
-      <div >
-        <h3>Carrinho:</h3>
+      <DentrodoCarrinho>
+
+      <div>
+        
+          <h3>Carrinho:</h3>
+
         <div>
-        <p>1x Viagem Cidade Espacial I </p>
+        <p>1x Viagem Cidade Espacial I 
+           <button>Remover</button>
+        </p>
+       
+        <p>1x Viagem à Lua 
+          <button>Remover</button>
+        </p>
+        
+        <p>1x Viagem à Marte 
         <button>Remover</button>
-  
-        </div>
-        <div>
-        <p>1x "Viagem à Lua  </p>
-        <button>Remover</button>
-  
-        </div>
-        <div>
-        <p>1x Viagem à Marte </p>
-        <button>Remover</button>
-  
+        </p>
         </div>
         
-  
-  
+         
         <p>Valor total R$ 100,00</p>
   
         <button>Finalizar compra</button>
+
   
       </div>
+       </DentrodoCarrinho>
     );
         
+
+
+
   }
     
+
 
 }
  
